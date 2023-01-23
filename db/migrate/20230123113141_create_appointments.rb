34b4673ea03be43946169fde6c106f88e468a27e
skyv26 <dev.aakashv@gmail.com>
2023-01-23 17:03:38 +0530

    set the `default` behavior of appointment table columns

--- db/migrate/20230123113141_create_appointments.rb
+++ db/migrate/20230123113141_create_appointments.rb
@@ -1,11 +1,11 @@
 class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :trainer, null: false, foreign_key: true
-      t.date :appointment_date
-      t.string :appointment_status
-      t.text :context
+      t.references :user, references: :users, null: false, foreign_key: { to_table: :users }
+      t.references :trainer, references: :trainers, null: false, foreign_key: { to_table: :trainers }
+      t.date :appointment_date, null: false
+      t.string :appointment_status, null: false, default: 'pending'
+      t.text :context, null: true
 
       t.timestamps
     end
