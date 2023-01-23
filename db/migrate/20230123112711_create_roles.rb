851575db46eb9eb32858e447f285a8335a624dc3
skyv26 <dev.aakashv@gmail.com>
2023-01-23 17:14:02 +0530

    Add the `defaul` value

--- db/migrate/20230123112711_create_roles.rb
+++ db/migrate/20230123112711_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
       t.references :user, references: :users, null: false, foreign_key: { to_table: :users }
-      t.string :role, null: false
+      t.string :role, null: false, default: 'user'
       t.text :security_question, null: true
       t.text :security_answer, null: true
 
