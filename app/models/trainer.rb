29df7132c83b0c0b22c9e70282dae80618462b00
Danny <barakadan421@gmail.com>
2023-01-23 15:14:09 +0300

    Add Trainer relations

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,6 @@
 class Trainer < ApplicationRecord
   belongs_to :user
+  has_many :appointments, destroy: :cascade
+  has_many :trainer_specialities, destroy: :cascade
+  has_many :specialities, through: :trainer_specialities
 end
