3c8b681da257de3a136a18bd2296a2488be30870
Danny <barakadan421@gmail.com>
2023-01-23 15:58:56 +0300

    Validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_one :roles, destroy: :cascade
   has_one :trainers, destroy: :cascade
   has_many :appointments, destroy: :cascade
+
+  validates :full_name, :date_of_birth, :address, :email_address, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :phone_number, presence: true,
+                           length: { minimum: 10, maximum: 16 }
 end
