a3e8cebb8f6af388ec09f35b2c8d6b44df2ffaf8
skyv26 <dev.aakashv@gmail.com>
2023-01-23 21:14:51 +0530

    Fix the `attribute` issue

--- app/models/speciality.rb
+++ app/models/speciality.rb
@@ -1,5 +1,5 @@
 class Speciality < ApplicationRecord
-  has_many :trainer_specialities, destroy: :cascade
+  has_many :trainer_specialities, dependent: :destroy
   has_many :trainers, through: :trainer_specialities
 
   validates :name, presence: true
