7b5581c32cf0f06ddf122e4b1fd0b629adf39b87
skyv26 <dev.aakashv@gmail.com>
2023-01-23 16:55:53 +0530

    set the `default` behavior of column

--- db/migrate/20230123112334_create_users.rb
+++ db/migrate/20230123112334_create_users.rb
@@ -1,16 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :full_name
-      t.date :date_of_birth
-      t.text :address
-      t.string :email_address
-      t.string :phone_number
-      t.text :health_info
-      t.decimal :height_in_meter
-      t.decimal :weight_in_kg
-      t.string :profile_pic
+      t.string :username, null: false
+      t.string :full_name, null: false
+      t.date :date_of_birth, null: false
+      t.text :address, null: false
+      t.string :email_address, null: false
+      t.string :phone_number, null: false
+      t.text :health_info, null: true
+      t.decimal :height_in_meter, null: true
+      t.decimal :weight_in_kg, null: true
+      t.string :profile_pic, null: true
 
       t.timestamps
     end
