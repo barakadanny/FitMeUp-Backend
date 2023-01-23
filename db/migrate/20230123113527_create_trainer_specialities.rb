f8a49c4e416d4e6ea9f3cc3dc65005897b844f38
skyv26 <dev.aakashv@gmail.com>
2023-01-23 17:06:28 +0530

    set the `default` behavior of trainer_speciality table columns

--- db/migrate/20230123113527_create_trainer_specialities.rb
+++ db/migrate/20230123113527_create_trainer_specialities.rb
@@ -1,8 +1,8 @@
 class CreateTrainerSpecialities < ActiveRecord::Migration[7.0]
   def change
     create_table :trainer_specialities do |t|
-      t.references :trainer, null: false, foreign_key: true
-      t.references :speciality, null: false, foreign_key: true
+      t.references :trainer, references: :trainers, null: false, foreign_key: { to_table: :trainers }
+      t.references :speciality,references: :specialities, null: false, foreign_key: { to_table: :specialities }
 
       t.timestamps
     end
