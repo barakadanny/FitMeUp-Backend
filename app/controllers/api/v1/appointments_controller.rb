c015354204c7b244c626d6b88c87b2c4e270584b
Danny <barakadan421@gmail.com>
2023-01-24 12:23:07 +0300

    Create appointment method

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::AppointmentsController < ApplicationController
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+  
+    if @appointment.save
+      render json: @appointment, status: :created
+    else
+      render json: { errors: @appointment.errors }, status: :unprocessable_entity
+    end
+  end
 
   private
-  
+
   def set_appointment
     @appointment = Appointment.find(params[:id])
   end
