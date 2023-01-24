785bb1bfe87eab75dc2f0a6fb270066090f86544
Danny <barakadan421@gmail.com>
2023-01-24 15:31:38 +0300

    Set_role method

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::RolesController < ApplicationController
 
+  private
+
+  def set_role
+    @role = Role.find(params[:id])
+  end
 
   def role_params
     params.require(:role).permit(:user_id, :role, :security_question, :security_answer)
