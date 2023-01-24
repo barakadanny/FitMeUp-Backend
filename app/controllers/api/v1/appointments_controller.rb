7cbb807525f4d286b74d14fc65b203cb917a8d4c
Danny <barakadan421@gmail.com>
2023-01-24 12:21:09 +0300

    Set appointment

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::AppointmentsController < ApplicationController
 
 
+  private
+  
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
   def appointment_params
     params.require(:appointment).permit(:user_id, :trainer_id, :appointment_date, :appointment_status, :context)
   end
