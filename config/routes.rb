2c7961567ee88b6f443b8142a11b260db8c80961
kibirige <kibirigejohn005@gmail.com>
2023-01-25 15:23:31 +0300

    add a route for displaying specilities of a given trainer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :trainers do
-        resources :specialities, only: [:index]
+        resources :trainer_specialities, only: [:index]
       end
     end
   end
