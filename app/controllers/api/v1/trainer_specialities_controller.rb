36c1ed533944b26ffd7f299331804f75c9350276
kibirige <kibirigejohn005@gmail.com>
2023-01-25 15:24:40 +0300

    add trainer_specialities controller

---
+++ app/controllers/api/v1/trainer_specialities_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::TrainerSpecialitiesController < ApplicationController
+    def index
+        @trainer = Trainer.find(params[:trainer_id])
+        render json: @trainer.specialities
+    end
+end
