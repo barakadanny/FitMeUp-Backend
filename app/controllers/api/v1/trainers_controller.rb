64117d4f87421ce59269d548dd5151e7c0878302
kibirige <kibirigejohn005@gmail.com>
2023-01-25 14:10:12 +0300

    add the create, update and destroy end points for trainers

--- app/controllers/api/v1/trainers_controller.rb
+++ app/controllers/api/v1/trainers_controller.rb
@@ -8,4 +8,40 @@ class TrainerController < ApplicationController
         @trainer = Trainer.find(params[:id])
         render json: @trainer
     end
+
+    def create
+        @trainer = Trainer.new (
+            user_id: params[:user_id],
+            price: params[:price],
+            bio: params[:bio]
+        )
+
+        if @trainer.save
+            render json: @trainer, status: :created
+        else
+            render json: { errors: @trainer.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @trainer = Trainer.find(params[:id])
+
+        if @trainer.update(user_id: params[:user_id], price: params[:price], bio: params[:bio])
+            render json: @trainer
+        else
+            render json: { errors: @trainer.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @trainers = Trainer.all
+        @trainer = Trainer.find(params[:id])
+
+        if @trainer != nil
+            @trainer.destroy
+            render json: @trainer
+        else
+            render json: { error: 'appointment not found' }, status: :not_found
+        end
+    end
 end
