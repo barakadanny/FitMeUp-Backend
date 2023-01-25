aecc8c26eff7cdb787464bb9e45a7f1ee519ebec
kibirige <kibirigejohn005@gmail.com>
2023-01-25 18:38:33 +0300

    remove the nested route from trainers resources and the controller

--- app/controllers/api/v1/trainer_specialities_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::TrainerSpecialitiesController < ApplicationController
-  def index
-    @trainer = Trainer.find(params[:trainer_id])
-    render json: @trainer.specialities
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :trainers do
-        resources :trainer_specialities, only: [:index]
-      end
+      resources :trainers
       resources :specialities, only: [:index]
     end
   end
