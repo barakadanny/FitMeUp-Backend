e5b3d1e304792d475c5c80de057265540819999e
skyv26 <dev.aakashv@gmail.com>
2023-01-29 21:58:57 +0530

    Add the `Serializers` for Role table

---
+++ app/serializers/role_serializer.rb
@@ -0,0 +1,12 @@
+class RoleSerializer < ActiveModel::Serializer
+  attributes :user
+  
+  def user
+    exclude = ['id', 'created_at', 'updated_at', 'security_question', 'security_answer']
+    role = JSON.parse(object.to_json).except(*exclude)
+    role[:id] = role['user_id']
+    role.delete('user_id')
+    user = JSON.parse(object.user.to_json).except(*exclude)
+    role.merge(user)
+  end
+end
