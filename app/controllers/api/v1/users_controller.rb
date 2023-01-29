985de2120f170cca9753f88b14f528d52112dc9c
skyv26 <dev.aakashv@gmail.com>
2023-01-29 17:09:18 +0530

    initiated the `User` endpoints creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,14 @@ module Api
   module V1
     class UsersController < ApplicationController
       def show
+        @user = User.find(user_params)
+        render json: @user
+      end
+
+      private
+
+      def user_params
+        params.permit(:username)
       end
     end
   end
