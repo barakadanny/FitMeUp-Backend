ea17db6da0cc55c3dc57a59861da275aff4adba0
skyv26 <dev.aakashv@gmail.com>
2023-01-29 17:40:14 +0530

    Update the `routes` for user-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post 'users/login', to: 'users#login'
       resources :trainers
       resources :specialities, only: [:index]
       resources :appointments
