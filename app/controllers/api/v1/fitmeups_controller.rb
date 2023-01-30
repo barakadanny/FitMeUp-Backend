f1d11662dc9423466e8760d6cc2e40ce52591173
skyv26 <dev.aakashv@gmail.com>
2023-01-30 15:56:10 +0530

    Add the `fitmeup#index` action method for controller

--- app/controllers/api/v1/fitmeups_controller.rb
+++ app/controllers/api/v1/fitmeups_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class FitMeUpsController < ApplicationController
+      def index
+        @user = User.find_by(username: params[:token])
+        render json: @user
+      end
     end
   end
 end
