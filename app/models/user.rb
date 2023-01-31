4c36e390873b8150a27078adf311bce4af1f9b56
skyv26 <dev.aakashv@gmail.com>
2023-01-31 14:55:00 +0530

    Add the `class_name` to the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_one :roles, dependent: :destroy
-  has_one :trainers, dependent: :destroy
-  has_many :appointments, dependent: :destroy
+  has_one :roles, dependent: :destroy, class_name: 'Role'
+  has_one :trainers, dependent: :destroy, class_name: 'Trainer'
+  has_many :appointments, dependent: :destroy, class_name: 'Appointment'
 
   validates :full_name, :date_of_birth, :address, :email_address, presence: true
   validates :username, presence: true, uniqueness: { case_sensitive: false }
