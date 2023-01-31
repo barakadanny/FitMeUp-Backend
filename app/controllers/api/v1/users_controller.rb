f5ce8bd428ec42bc94c8baee9944587f4a1051c0
skyv26 <dev.aakashv@gmail.com>
2023-01-31 21:24:04 +0530

    Add `custom_serializer` in user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,31 @@ module Api
 
       def show
         @user = User.find(params[:id])
-        render json: @user
+        render json: modify_user(@user)
+      end
+
+      private
+
+      def modify_user(user_obj)
+        exclude = ['created_at', 'updated_at', 'security_question', 'security_answer', 'user_id']
+        role_json = JSON.parse(user_obj.roles.to_json).except(*exclude, 'id')
+        user_json = JSON.parse(user_obj.to_json).except(*exclude)
+        appointments = user_obj.appointments.map do |appointment|
+          obj = JSON.parse(appointment.to_json).except(*exclude)
+          appointement_trainer = Trainer.find(obj['trainer_id'])
+          obj.delete('trainer_id')
+          trainer_user_detail = JSON.parse(appointement_trainer.user.to_json).slice('full_name', 'profile_pic')
+          appointement_trainer_obj = {
+            id: appointement_trainer.id,
+            appointment_url: "#{request.base_url}/api/v1/appointments/#{appointement_trainer.id}"
+          }
+          filtered_data = JSON.parse(appointement_trainer.to_json).except(*exclude)
+          appointement_trainer_obj.merge(filtered_data, trainer_user_detail, obj)
+        end
+        appointment_obj = {
+          appointments:,
+        }
+        user_json.merge(role_json, appointment_obj)
       end
     end
   end
