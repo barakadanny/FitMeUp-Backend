055cfef67c9d9ccc6582b20dc825553c2e0af9fe
skyv26 <dev.aakashv@gmail.com>
2023-01-31 20:12:19 +0530

    Remove the `redundant` code

--- app/controllers/api/v1/trainers_controller.rb
+++ app/controllers/api/v1/trainers_controller.rb
@@ -13,42 +13,6 @@ class Api::V1::TrainersController < ApplicationController
     render json: modify_trainer(@trainer)
   end
 
-  # def create
-  #   @trainer = Trainer.new(
-  #     user_id: params[:user_id],
-  #     price: params[:price],
-  #     bio: params[:bio]
-  #   )
-
-  #   if @trainer.save
-  #     render json: @trainer, status: :created
-  #   else
-  #     render json: { errors: @trainer.errors }, status: :unprocessable_entity
-  #   end
-  # end
-
-  # def update
-  #   @trainer = Trainer.find(params[:id])
-
-  #   if @trainer.update(user_id: params[:user_id], price: params[:price], bio: params[:bio])
-  #     render json: @trainer
-  #   else
-  #     render json: { errors: @trainer.errors }, status: :unprocessable_entity
-  #   end
-  # end
-
-  # def destroy
-  #   @trainers = Trainer.all
-  #   @trainer = Trainer.find(params[:id])
-
-  #   if @trainer.nil?
-  #     render json: { error: 'trainer not found' }, status: :not_found
-  #   else
-  #     @trainer.destroy
-  #     render json: @trainer
-  #   end
-  # end
-  
   private
 
   def modify_trainer(trainer_obj)
