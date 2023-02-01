bad019ead483a84819940c5a5f380179efe37290
skyv26 <dev.aakashv@gmail.com>
2023-02-01 15:54:19 +0530

    Add the `password_digest` column

--- db/migrate/20230123112334_create_users.rb
+++ db/migrate/20230123112334_create_users.rb
@@ -5,6 +5,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :full_name, null: false
       t.date :date_of_birth, null: false
       t.text :address, null: false
+      t.string :email_address, null: false
+      t.string :password_digest, null: false
       t.string :phone_number, null: false
       t.text :health_info, null: true
       t.decimal :height_in_meter, null: true
