ac48e4ae822d8a1bb1c122e8b892d12bab637c9f
skyv26 <dev.aakashv@gmail.com>
2023-02-01 23:03:32 +0530

    Update project with `api_only`

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module FitmeupBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.api_only = true
+    config.api_only = true
   end
 end
