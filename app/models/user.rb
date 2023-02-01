7fb9073dff8c4a8790e0aa7c1f18a03ceffe82a1
skyv26 <dev.aakashv@gmail.com>
2023-02-01 12:52:47 +0530

    Add the devise attributes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_one :roles, dependent: :destroy, class_name: 'Role'
   has_one :trainers, dependent: :destroy, class_name: 'Trainer'
   has_many :appointments, dependent: :destroy, class_name: 'Appointment'
