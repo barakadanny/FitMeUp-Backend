d6a2d5b97785745a2547fdbc44689f1c4b6a2aad
skyv26 <dev.aakashv@gmail.com>
2023-02-01 15:56:54 +0530

    generate the `new` schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_073147) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_113527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_073147) do
     t.string "full_name", null: false
     t.date "date_of_birth", null: false
     t.text "address", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
     t.string "phone_number", null: false
     t.text "health_info"
     t.decimal "height_in_meter"
@@ -72,15 +74,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_073147) do
     t.string "profile_pic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "jti", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "appointments", "trainers"
