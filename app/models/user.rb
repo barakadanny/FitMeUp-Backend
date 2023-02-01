3d37512f773e05376e86b2b5577a96b54a58daaa
skyv26 <dev.aakashv@gmail.com>
2023-02-01 15:58:43 +0530

    Add the `has_secure_password` attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
-  include Devise::JWT::RevocationStrategies::JTIMatcher
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
+  has_secure_password
   has_one :roles, dependent: :destroy, class_name: 'Role'
   has_one :trainers, dependent: :destroy, class_name: 'Trainer'
   has_many :appointments, dependent: :destroy, class_name: 'Appointment'
