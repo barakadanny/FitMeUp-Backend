d4a3e5d79a3386c3a8fe94a495010c48d9a6af6d
kibirige <kibirigejohn005@gmail.com>
2023-02-02 22:01:13 +0300

    comment the authenticate_request in specialites controller

--- app/controllers/api/v1/specialities_controller.rb
+++ app/controllers/api/v1/specialities_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SpecialitiesController < ApplicationController
-  before_action :authenticate_request
+  # before_action :authenticate_request
 
   def index
     @specialities = Speciality.all
