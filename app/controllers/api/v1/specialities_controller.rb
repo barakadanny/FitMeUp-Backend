e3c52cbcd3b0d81fb8e4e07159339ca66e87bc79
kibirige <kibirigejohn005@gmail.com>
2023-02-02 22:30:08 +0300

    define the logic of destorying a spciality

--- app/controllers/api/v1/specialities_controller.rb
+++ app/controllers/api/v1/specialities_controller.rb
@@ -7,7 +7,23 @@ class Api::V1::SpecialitiesController < ApplicationController
   end
 
   def create
+    @speciality = Speciality.new(speciality_params)
 
+    if @speciality.save
+      render json: @speciality, status: :created
+    else
+      render json: { errors: @speciality.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @speciality = Speciality.find(params[:id])
+    if @speciality.present?
+      @speciality.destroy
+      render json: { message: 'speciality deleted successfully' }, status: :ok
+    else
+       render json: { error: 'spciality not found' }, status: :not_found
+    end
   end
 end
 
