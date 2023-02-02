13dae27e9d233e0674b89147aef83a8c1889f854
skyv26 <dev.aakashv@gmail.com>
2023-02-02 19:21:06 +0530

    Delete the `unnecessary code and file`

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -1,9 +1,8 @@
 require 'jwt'
 module JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
-  # SECRET_KEY = Rails.application.secret_key_base
 
-  def self.encode(payload, exp = 120.minutes.from_now)
+  def self.encode(payload, exp = 1.minutes.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
